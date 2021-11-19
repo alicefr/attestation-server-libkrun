90b375b06c59b5d5c6a7763998bcdbbf8aa1901b
Alice Frosi <afrosi@redhat.com>
2021-11-19 11:15:35 +0100

    Fix formatting of the kernel command line

--- register-image/entrypoint.sh
+++ register-image/entrypoint.sh
@@ -14,7 +14,7 @@ buildah images $image || buildah pull $image
 sha=$(buildah inspect --format='{{.FromImageDigest}}' $image)
 prolog_kernel="reboot=k panic=-1 panic_print=0 pci=off nomodules console=hvc0 quiet rw no-kvmapf init=/bin/sh virtio_mmio.device=4K@0xd0000000:5 virtio_mmio.device=4K@0xd0001000:6 virtio_mmio.device=4K@0xd0002000:7 virtio_mmio.device=4K@0xd0003000:8 swiotlb=65536 KRUN_WORKDIR=/"
 
-kernel_cmdline="KRUN_CFG=$CPUS:$RAM $prolog_kernel KRUN_PASS=$password KRUN_INIT=$3 -- \0"
+kernel_cmdline="KRUN_CFG=$CPUS:$RAM $prolog_kernel KRUN_PASS=$password KRUN_INIT=$3 -- \\0"
 
 curl -X POST $image_reg_svc \
    -H 'Content-Type: application/json' \
