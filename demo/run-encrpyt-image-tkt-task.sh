2f00304ae7695c316a27d44d2eac1857c5eb88d7
Alice Frosi <afrosi@redhat.com>
2021-11-19 11:48:46 +0100

    Fix registry name for the encrypt image tekton task

--- demo/run-encrpyt-image-tkt-task.sh
+++ demo/run-encrpyt-image-tkt-task.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+OUTPUT_NS=untrusted
+# Create untrusted ns where the encrypted image will be deployed for the demo
+oc new-project $OUTPUT_NS
+
 SA=tekton-encryp-images
-OUTPUT_NS=
+REGISTRY=image-registry.openshift-image-registry.svc:5000
 # Run the tekton task for encrypting the image
 tkn task  start encrypt-image -p input-image=fedora:latest \
-        -p output-image="$INSECURE_NS/demo-encrypted-image:latest" \
+        -p output-image="$REGISTRY/$OUTPUT_NS/demo-encrypted-image:latest" \
         --use-param-defaults \
 	-p namespace-tekton-task="attestation" \
         -p password=myamazingpassword \
