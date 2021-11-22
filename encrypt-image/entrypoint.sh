8d00162ef51582e6b9e094d0d3271ce40db56d8f
Alice Frosi <afrosi@redhat.com>
2021-11-22 07:18:08 -0500

    Fix script

--- encrypt-image/entrypoint.sh
+++ encrypt-image/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 IMAGE=$1
 ENCRYPT_IMAGE=$2
@@ -7,29 +7,42 @@ PASSWORD=$3
 OUTPUT=disk.img
 CONT=temporary-container
 OUTDIR=/tmp/output
+ROOT_DIR=/tmp/rootfs
 DISK=$OUTDIR/$OUTPUT
+DISK_SIZE=1G
+CRYPT_PARTITION=root
 
 buildah rm $CONT
+cryptsetup luksClose $CRYPT_PARTITION
+rm $DISK
+set -e
 
-set -xe
-
-mkdir -p $OUTDIR
+mkdir -p $OUTDIR $ROOT_DIR
+fallocate -l $DISK_SIZE $DISK
+# encrypt the image with the LUKS passphrase
+DEVICE=$(losetup  -f  --show $DISK)
+echo "YES" | echo "$PASSWORD"| cryptsetup luksFormat -y -v --cipher aegis128-random --key-size 128 --integrity aead --type luks2 $DEVICE
+echo "$PASSWORD" | cryptsetup luksOpen $DEVICE $CRYPT_PARTITION
+mkfs.ext4 /dev/mapper/$CRYPT_PARTITION
+mount /dev/mapper/$CRYPT_PARTITION $ROOT_DIR
 buildah from --name $CONT $IMAGE
 dir=$(buildah mount $CONT)
 cd $dir
-find . | cpio -o -c -R root:root | gzip -9 >  $DISK
-sleep 2
-# encrypt the image with the LUKS passphrase
-DEVICE=$(losetup  -f  --show $DISK)
-echo "YES" | echo "$PASSWORD" | cryptsetup -y -v --type luks2 luksFormat $DEVICE
+cp -r * $ROOT_DIR
+# Clean up
+umount $ROOT_DIR
+cryptsetup luksClose $CRYPT_PARTITION
 losetup -d $DEVICE
 buildah rm $CONT
 
 
 # TODO generate fake entrypoint
+touch $OUTDIR/entrypoint.sh
+chmod +x $OUTDIR/entrypoint.sh
 
 # Create final image
 buildah from --name $CONT scratch 
 buildah copy $CONT $DISK /
+buildah copy $CONT $OUTDIR/entrypoint.sh /
 # TODO copy fake entrypoint in the final image
 buildah commit --rm $CONT $ENCRYPT_IMAGE
