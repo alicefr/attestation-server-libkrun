2b299aee6bd56a39858bc3cd406360369f763643
Alice Frosi <afrosi@redhat.com>
2021-11-29 02:41:08 -0500

    Add script to start crun and memory

--- start-podman-krun-sev.sh
+++ start-podman-krun-sev.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -x
 
 IP=$(sudo podman container inspect --format '{{ .NetworkSettings.IPAddress }}' attestation-server)
-sudo podman run -ti --runtime /usr/local/bin/crun \
+sudo  podman run -ti --runtime /usr/local/bin/crun \
+       -m 3072M \
        --device /dev/kvm \
        --device /dev/sev \
-       --ulimit memlock=4294967296:4294967296 \
        --volume /tmp/libkrun-sev.chain:/tmp/libkrun-sev.chain \
-       --runtime-flag "debug" \
        --entrypoint "/entrypoint.sh" \
        --annotation run.oci.handler="krun" \
        --annotation krun/attestation="http://$IP:8081/untrusted" \
-       --annotation krun/image="fedora-encrypt-luks1@sha256:a879b1f277c04b03ac912980aa9b3a2562a2f4fcbc5c8606c58b0fa40d93ec57" \
+       --annotation krun/image="localhost/fedora-encrypt-luks1@sha256:6c4bec5d6ad2c3deff4c4a59f10188bb3d6cc1033d0d603b4d08ee67d7a449aa" \
        fedora-encrypt-luks1
-
